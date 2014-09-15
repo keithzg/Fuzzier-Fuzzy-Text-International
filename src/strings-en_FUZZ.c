47455fee8f71660e338a3787050c465c65758ac6
Keith Z-G <keithzg@gmail.com>
2014-09-15 15:44:37 -0600

    Initial strings-en_FUZZ.c
    Replace hours with various, fuzzier descriptors; blank out list of connecting phrases.

---
+++ src/strings-en_FUZZ.c
@@ -0,0 +1,54 @@
+#include "strings-en_FUZZ.h"
+
+const char* const HOURS_EN_US[] = {
+  // AM hours
+  "mid night",
+  "mid night",
+  "night",
+  "night",
+  "night",
+  "night",
+  "night",
+  "morn ing",
+  "morn ing",
+  "morn ing",
+  "morn ing",
+  "morn ing",
+
+  // PM hours
+  "noon",
+  "noon",
+  "after noon",
+  "after noon",
+  "after noon",
+  "after noon",
+  "evening",
+  "evening",
+  "evening",
+  "evening",
+  "evening",
+  "night"
+};
+
+/**
+ * The string "$1" will be replaced with the current hour (e.g., "three"
+ * at 3:45).  The string "$2" will be replaced with the *next* hour
+ * (e.g., "four" at 3:45).
+ *
+ * A "*" character before a word makes that word bold.
+ */
+const char* const RELS_EN_US[] = {
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  ""
+};
+
